8eb20e8cca987f95e09f6822b94135476c9fff15
Achy <achymake@gmail.com>
2023-01-22 15:16:20 +0100

    particle fix

--- src/main/java/net/achymake/chunkclaim/command/sub/Claim.java
+++ src/main/java/net/achymake/chunkclaim/command/sub/Claim.java
@@ -5,10 +5,7 @@ import net.achymake.chunkclaim.command.ChunkSubCommand;
 import net.achymake.chunkclaim.config.Config;
 import net.achymake.chunkclaim.config.MessageConfig;
 import net.achymake.chunkclaim.settings.ChunkSettings;
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 
 import java.text.MessageFormat;
@@ -43,7 +40,7 @@ public class Claim extends ChunkSubCommand {
                 if (ChunkClaim.econ.getBalance(player) >= Config.get().getDouble("economy.cost")) {
                     ChunkClaim.econ.withdrawPlayer(player, Config.get().getDouble("economy.cost"));
                     ChunkSettings.claimChunk(player);
-                    player.spawnParticle(Particle.valueOf(Config.get().getString("settings.claim.particle.type")), player.getLocation().getChunk().getBlock(8, 64, 8).getX(), player.getLocation().add(0,3,0).getBlockY(), player.getLocation().getChunk().getBlock(8, 64, 8).getZ(), Config.get().getInt("settings.claim.particle.count"), Config.get().getDouble("settings.claim.particle.offsetX"), Config.get().getDouble("settings.claim.particle.offsetY"), Config.get().getDouble("settings.claim.particle.offsetZ"), 0);
+                    chunkParticle(player);
                     player.playSound(player.getLocation(), Sound.valueOf(Config.get().getString("settings.claim.sound.type")), Float.parseFloat(Config.get().getString("settings.claim.sound.volume")), Config.get().getInt("settings.claim.sound.pitch"));
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&',MessageFormat.format(MessageConfig.get().getString("command-claim"),ChunkClaim.econ.format(Config.get().getDouble("economy.cost")))));
                 } else {
@@ -51,5 +48,18 @@ public class Claim extends ChunkSubCommand {
                 }
             }
         }
+    }
+    private void chunkParticle(Player player){
+        int a = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(15,0,8).getX(),player.getLocation().getChunk().getBlock(15,0,8).getZ()).getLocation().getBlockY();
+        Location locationSouth = new Location(player.getWorld(),player.getLocation().getChunk().getBlock(15, 0, 8).getX(), a, player.getLocation().getChunk().getBlock(15, 0, 8).getZ());
+        int b = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(8,0,15).getX(),player.getLocation().getChunk().getBlock(8,0,15).getZ()).getLocation().getBlockY();
+        Location locationEast = new Location(player.getWorld(), player.getLocation().getChunk().getBlock(8, 0, 15).getX(), b, player.getLocation().getChunk().getBlock(8, 0, 15).getZ());
+        int c = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(8,0,0).getX(),player.getLocation().getChunk().getBlock(8,0,0).getZ()).getLocation().getBlockY();
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.claim.particle.type")), player.getLocation().getChunk().getBlock(8, 0, 0).getX(), c, player.getLocation().getChunk().getBlock(8, 0, 0).getZ(), 250, 4, 12, 0, 0);
+        int d = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(0,0,8).getX(),player.getLocation().getChunk().getBlock(0,0,8).getZ()).getLocation().getBlockY();
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.claim.particle.type")), player.getLocation().getChunk().getBlock(0, 0, 8).getX(), d, player.getLocation().getChunk().getBlock(0, 0, 8).getZ(), 250, 0, 12, 4, 0);
+
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.claim.particle.type")),locationSouth.add(1,0,0), 250, 0, 12, 4, 0);
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.claim.particle.type")),locationEast.add(0,0,1), 250, 4, 12, 0, 0);
     }
 }
--- src/main/java/net/achymake/chunkclaim/command/sub/Unclaim.java
+++ src/main/java/net/achymake/chunkclaim/command/sub/Unclaim.java
@@ -5,10 +5,7 @@ import net.achymake.chunkclaim.command.ChunkSubCommand;
 import net.achymake.chunkclaim.config.Config;
 import net.achymake.chunkclaim.config.MessageConfig;
 import net.achymake.chunkclaim.settings.ChunkSettings;
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 
 import java.text.MessageFormat;
@@ -35,7 +32,7 @@ public class Unclaim extends ChunkSubCommand {
         if (ChunkSettings.isClaimed(chunk)){
             if (ChunkSettings.isOwner(player.getUniqueId(),chunk)){
                 ChunkSettings.delete(chunk);
-                player.spawnParticle(Particle.valueOf(Config.get().getString("settings.un-claim.particle.type")),player.getLocation().getChunk().getBlock(8,64,8).getX(),player.getLocation().add(0,3,0).getBlockY(),player.getLocation().getChunk().getBlock(8,64,8).getZ(), Config.get().getInt("settings.un-claim.particle.count"), Config.get().getDouble("settings.un-claim.particle.offsetX"), Config.get().getDouble("settings.un-claim.particle.offsetY"), Config.get().getDouble("settings.un-claim.particle.offsetZ"), 0);
+                chunkParticle(player);
                 player.playSound(player.getLocation(), Sound.valueOf(Config.get().getString("settings.un-claim.sound.type")),Float.parseFloat(Config.get().getString("settings.un-claim.sound.volume")), Config.get().getInt("settings.un-claim.sound.pitch"));
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',MessageFormat.format(MessageConfig.get().getString("command-unclaim"),ChunkClaim.econ.format(Config.get().getDouble("economy.refund")))));
             }else{
@@ -44,5 +41,18 @@ public class Unclaim extends ChunkSubCommand {
         }else{
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', MessageConfig.get().getString("error-chunk-already-unclaimed")));
         }
+    }
+    private void chunkParticle(Player player){
+        int a = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(15,0,8).getX(),player.getLocation().getChunk().getBlock(15,0,8).getZ()).getLocation().getBlockY();
+        Location locationSouth = new Location(player.getWorld(),player.getLocation().getChunk().getBlock(15, 0, 8).getX(), a, player.getLocation().getChunk().getBlock(15, 0, 8).getZ());
+        int b = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(8,0,15).getX(),player.getLocation().getChunk().getBlock(8,0,15).getZ()).getLocation().getBlockY();
+        Location locationEast = new Location(player.getWorld(), player.getLocation().getChunk().getBlock(8, 0, 15).getX(), b, player.getLocation().getChunk().getBlock(8, 0, 15).getZ());
+        int c = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(8,0,0).getX(),player.getLocation().getChunk().getBlock(8,0,0).getZ()).getLocation().getBlockY();
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.un-claim.particle.type")), player.getLocation().getChunk().getBlock(8, 0, 0).getX(), c, player.getLocation().getChunk().getBlock(8, 0, 0).getZ(), 250, 4, 12, 0, 0);
+        int d = player.getWorld().getHighestBlockAt(player.getLocation().getChunk().getBlock(0,0,8).getX(),player.getLocation().getChunk().getBlock(0,0,8).getZ()).getLocation().getBlockY();
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.un-claim.particle.type")), player.getLocation().getChunk().getBlock(0, 0, 8).getX(), d, player.getLocation().getChunk().getBlock(0, 0, 8).getZ(), 250, 0, 12, 4, 0);
+
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.un-claim.particle.type")),locationSouth.add(1,0,0), 250, 0, 12, 4, 0);
+        player.spawnParticle(Particle.valueOf(Config.get().getString("settings.un-claim.particle.type")),locationEast.add(0,0,1), 250, 4, 12, 0, 0);
     }
 }
